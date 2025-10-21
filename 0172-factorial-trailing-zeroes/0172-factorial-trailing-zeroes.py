344d82b9da24b0f16e3a2863625a2eb237485400
KayBee1880 <boatengkwaku1965@gmail.com>
2025-10-20 22:27:35 -0500

    Time: 0 ms (100%), Space: 18 MB (15.37%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -0,0 +1,10 @@
+class Solution: #Time complexity = O(log to the base of 5 *N) and space = O(1)
+    def trailingZeroes(self, n: int) -> int:
+        count = 0
+        power_of_5 = 5
+        while n >= power_of_5:
+            count += n//power_of_5
+            power_of_5 *= 5
+        return count
+
+        

aed83d1be36f591dbed54e6b072a94f47b93ed35
Kwaku Boateng <boatengkwaku1965@gmail.com>
2025-12-26 10:57:54 -0600

    Time: 67 ms (70.26%), Space: 18.2 MB (70.38%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,21 +1,30 @@
+from collections import Counter, defaultdict
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(t) > len(s): return "" 
-        min_window = ""
-        min_length = float("inf")
+        if not s or not t or len(s) < len(t): 
+            return ""
+        window_count = defaultdict(int)
         count_t = Counter(t)
-        for i in range(len(s)): 
-            for j in range(i + len(t), len(s) + 1): 
-                substring = s[i:j]
-                sub_count = Counter(substring)
-                valid = True
-                for key, val in count_t.items(): 
-                    if sub_count[key] < val: 
-                        valid = False 
-                        break
-                if valid and len(substring) < min_length: 
-                    min_length = len(substring)
-                    min_window = substring
-        return min_window
-
+        left = 0
+        min_left = 0
+        min_len = float("inf")
+        required = len(count_t)
+        formed = 0
+        for right in range(len(s)): 
+            char = s[right]
+            window_count[char] += 1
+            if char in count_t and count_t[char] == window_count[char]: 
+                formed += 1
+            while left <= right and formed == required: 
+                if right - left + 1 < min_len and formed == required: 
+                    min_len = right - left +1
+                    min_left = left 
+                left_char = s[left]
+                window_count[left_char] -= 1
+                if left_char in count_t and count_t[left_char] > window_count[left_char]: 
+                    formed -= 1
+                left += 1
+        return "" if min_len == float("inf") else s[min_left: min_left + min_len]
+            
+            
 

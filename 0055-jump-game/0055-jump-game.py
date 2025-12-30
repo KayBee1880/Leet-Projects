8885c872c10204d1afe5bf30abea0e860791a506
Kwaku Boateng <boatengkwaku1965@gmail.com>
2025-12-29 19:32:35 -0600

    Time: 36 ms (43.53%), Space: 18.6 MB (41.46%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,10 +1,21 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
+        """
+        Check if the last index can be reached
+        Input: 
+            - list[int]
+        Output: 
+            - bool val [True / False]
+        Edge Case: 
+            - If list[int] is empty 
+            - If jumps get stuck in the middle of the array
+            - If we get to a point in the traversal where the jump can't take us to the last index
+            -
+        """
         farthest = 0
-        for i in range(len(nums)):
-            if i > farthest: 
-                return False
+        for i in range(len(nums)): 
+            if i > farthest: return False
             farthest = max(farthest, i + nums[i])
-            if farthest >= len(nums) - 1:
+            if farthest >= len(nums) - 1: 
                 return True 
-        return False
+        return False 

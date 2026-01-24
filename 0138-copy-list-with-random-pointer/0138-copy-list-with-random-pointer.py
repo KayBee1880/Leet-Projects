9c143dba2bf03ba014ff547b49acb362f34cc4ef
Kwaku Boateng <boatengkwaku1965@gmail.com>
2026-01-24 05:55:09 -0600

    Time: 38 ms (50.71%), Space: 13.3 MB (45.65%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -1,50 +1,27 @@
 """
 # Definition for a Node.
 class Node:
-    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+    def __init__(self, x, next=None, random=None):
         self.val = int(x)
         self.next = next
         self.random = random
 """
 
-class Solution:
-    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+class Solution(object):
+    def copyRandomList(self, head):
+        """
+        :type head: Node
+        :rtype: Node
+        """
         if not head: return None 
+        old_to_new = {}
         curr = head 
-        while curr:
-            new_node = Node(curr.val)
-            new_node.next = curr.next 
-            curr.next = new_node
-            curr = new_node.next 
-        curr = head
-        while curr:
-            if curr.random:
-                curr.next.random = curr.random.next 
-            curr = curr.next.next 
-
+        while curr: 
+            old_to_new[curr] = Node(curr.val)
+            curr = curr.next 
         curr = head 
-        copy_head = head.next 
-        while curr:
-            copy = curr.next
-            curr.next = copy.next
-            copy.next = copy.next.next if copy.next else None 
-            curr = curr.next
-        return copy_head 
-        
-
-
-    #Truthfully i prefer this particular approach using the hashmaps
-        oldtocopy = {None: None}
-        curr = head 
-        while curr:
-            copy = Node(curr.val)
-            oldtocopy[curr] = copy
-            curr = curr.next
-        
-        curr = head 
-        while curr:
-            copy = oldtocopy[curr]
-            copy.next = oldtocopy[curr.next]
-            copy.random = oldtocopy[curr.random]
-            curr = curr.next
-        return oldtocopy[head]
+        while curr: 
+            old_to_new[curr].next = old_to_new.get(curr.next)
+            old_to_new[curr].random = old_to_new.get(curr.random)
+            curr = curr.next 
+        return old_to_new[head]

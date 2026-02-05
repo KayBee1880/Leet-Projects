ca4625f8cc8a52b4226d8fdf69cb5681318acaf4
Kwaku Boateng <boatengkwaku1965@gmail.com>
2026-02-05 09:07:54 -0600

    Time: 0 ms (100%), Space: 18 MB (61.63%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,7 +1,6 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        s = s.strip()
-        list_s = s.split()
-        list_s.reverse()
-        return " ".join(list_s)
+        words = s.strip().split()
+        reversed_words = words[::-1]
+        return " ".join(reversed_words)
         

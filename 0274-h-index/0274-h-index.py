7be470cef97c1def1c996b2fef39c925ddba349e
Kwaku Boateng <boatengkwaku1965@gmail.com>
2026-02-04 20:27:22 -0600

    Time: 0 ms (100%), Space: 12.7 MB (74.8%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,7 +1,18 @@
-class Solution:
-    def hIndex(self, citations: List[int]) -> int:
-        citations.sort(reverse = True)
-        for i, citation in enumerate(citations): 
-            if citation < i + 1: 
-                return i 
-        return len(citations)
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        n = len(citations)
+        bucket = [0] * (n+1)
+        for c in citations: 
+            if c >= n: 
+                bucket[n] += 1
+            else: 
+                bucket[c] += 1
+        total = 0
+        for h in range(n, -1, -1): 
+            total += bucket[h]
+            if total >= h: 
+                return h

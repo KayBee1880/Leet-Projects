7213cacb9921a7fe057539a36fa36f84b75fa0bc
Kwaku Boateng <boatengkwaku1965@gmail.com>
2026-02-16 12:37:46 -0600

    Time: 343 ms (68.56%), Space: 19.7 MB (18.12%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.py
+++ 0454-4sum-ii/0454-4sum-ii.py
@@ -1,12 +1,8 @@
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
-        sum_Map = Counter(a+b for a in nums1 for b in nums2)
+        ab = Counter(a+b for a in nums1 for b in nums2)
         count = 0
-        for c in nums3:
-            for d in nums4:
-                complement = -(c+d)
-                count += sum_Map.get(complement,0)
-
+        for c in nums3: 
+            for d in nums4: 
+                count += ab.get(-(c+d), 0)
         return count
-    
-    

391a947904eded3e25c3f3e0cdea8fa5c31a84f4
Kwaku Boateng <boatengkwaku1965@gmail.com>
2026-07-29 09:38:52 -0500

    Time: 88 ms (20.17%), Space: 90.7 MB (33.03%) - LeetHub - Time: O(n^2), Space: O(n)

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not inorder or not preorder: return None 
+        root_val = preorder[0]
+        root = TreeNode(root_val)
+        root_index = inorder.index(root_val)
+        root.left = self.buildTree( preorder[1:root_index+1], inorder[:root_index])
+        root.right = self.buildTree(preorder[root_index + 1:], inorder[root_index+1:])
+        return root
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

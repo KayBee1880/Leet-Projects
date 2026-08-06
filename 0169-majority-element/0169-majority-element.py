d8913ca7f3ec3a4e61fca4dabe3cbd293da45209
Kwaku Boateng <boatengkwaku1965@gmail.com>
2026-08-05 21:53:30 -0500

    Time: 3 ms (84.92%), Space: 21.2 MB (47.88%) - LeetHub - Time: O(n), Space: O(n)

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,7 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        freq = Counter(nums)
-        for key, val in freq.items(): 
-            if val == max(list(freq.values())):
-                return key 
-        
+        count = Counter(nums)
+        max_count = 0
+        majority = nums[0]
+        for key, val in count.items(): 
+            if val > max_count:
+                max_count = val 
+                majority = key
+        return majority
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

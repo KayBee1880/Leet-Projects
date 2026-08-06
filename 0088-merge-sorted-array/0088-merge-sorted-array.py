50b105aea616f44c62615c14d46b0bd4d4d26310
Kwaku Boateng <boatengkwaku1965@gmail.com>
2026-08-05 20:43:42 -0500

    Time: 0 ms (100%), Space: 19.2 MB (77.02%) - LeetHub - Time: O(m + n), Space: O(1)

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,18 +3,21 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        #With this i will start with pointers by declaring pointers for nums1, nums2 and nums1 again 
-        p1 = m-1
-        p2 = n-1
-        p = m+n-1
-
-        while p1>=0 and p2>=0:
-            if nums1[p1] > nums2[p2]:
+        p1 = m -1
+        p2 = n -1 
+        p = m + n -1 
+        while p1 >= 0 and p2 >= 0: 
+            if nums1[p1] > nums2[p2]: 
                 nums1[p] = nums1[p1]
-                p1 -= 1 
-            else:
+                p1 -= 1
+            else: 
                 nums1[p] = nums2[p2]
-                p2 -= 1 
+                p2 -= 1
             p -= 1
-           
         nums1[:p2+1] = nums2[:p2+1]
+         
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

9627762a7a55bb36702729ac181862e147fa52b8
Kwaku Boateng <boatengkwaku1965@gmail.com>
2026-08-06 13:58:30 -0500

    Time: 12 ms (96.95%), Space: 25.3 MB (85.46%) - LeetHub - Time: O(n), Space: O(n)

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,40 +1,19 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        """
-        Doc String: 
-        An Algorithm that returns an array "answer" such that answer[i] == product of nums[:] except nums[i]
-        Input: 
-            - nums : List[int]
-        Output: 
-            - answer : List[int]
-        Edge case: 
-            - if zero(0) is an element 
-            - No division is allowed
-            - if len(nums) < 2 ??
-            - If all elements are same non-neg number
-            - more than one zero in nums[:]
-        Constraints; 
-            - Guaranteed that input fits in a 32 bit integer
-
-        Thought Process:
-            - n = len(nums) 
-            - #if n <2: return [1] "This is void since question explains that the n >= 2"
-            - answer = [1] * n
-            - General formula: answer[i] = (product to the left of i) * (product of all elements to the right of i)
-
-        """
         n = len(nums)
-        answer = [1] * n
-        # First pass: Prefix product
-        left = 1 #this records the prefix product 
+        res = [1] * n 
+        prefix = 1
         for i in range(n): 
-            answer[i] = left 
-            left *= nums[i]
-        # Second pass: suffix product 
-        right = 1
-        for i in reversed(range(n)): 
-            answer[i]*=right
-            right *= nums[i]
-        return answer
+            res[i] = prefix
+            prefix *= nums[i]
+        
+        suffix = 1
+        for i in range(n-1, -1, -1): 
+            res[i] *= suffix
+            suffix*= nums[i]
+        
+        return res
 
-
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
